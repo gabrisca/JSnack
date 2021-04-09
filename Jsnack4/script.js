3057c55b40937918695737f96689dc08ed1d1d17
gabrisca <gabrielescarparo1985@gmail.com>
2021-04-09 12:21:34 +0200

    inserisco commenti

--- Jsnack4/script.js
+++ Jsnack4/script.js
@@ -1,5 +1,5 @@
 // IN UN ARRAY SONO CONTENUTI I NOMI DEGLI INVITATI ALLA FESTA DEL GRANDE GATSBY. CHIEDI ALL'UTENTE IL SUO NOME E COMUNICAGLI SE PUP' PARTECIPARE O NO ALLA FESTA
-var myName = prompt("Lei è il signor...?");
+var myName = prompt("Lei è il signor...?"); //variabile myName per chiedere chi è?
 
 var listaInvitati = [
   "Nick Carraway",
@@ -8,19 +8,19 @@ var listaInvitati = [
   "Tom Buchanan",
   "George Wilson",
   "Pino"
-];
+]; // array con lista invitati alla festa
 
-var foundIt = false;
+var foundIt = false; // variabile trovato di default false
 
-for (var i = 0; i < listaInvitati.length; i++){
-  var invitato = listaInvitati[i];
+for (var i = 0; i < listaInvitati.length; i++) {
+  var invitato = listaInvitati[i]; // variabile invitato uguale a uno dei nomi della lista
   if (invitato === myName) {
     foundIt = true;
-  }
-}
+  } // se var invitato uguale a var myName(nome dato dalla persona all'inizio) > foundIt diventa True
+} // ciclo FOR
 
 if (foundIt) {
-  alert("Benvenuto signore/a");
+  alert("Benvenuto signore/a"); // se il nome è sulla lista do il benvenuto
 } else {
-  alert("Mi dispiace. Il suo nome non è nella lista. Arrivederci")
+  alert("Mi dispiace. Il suo nome non è nella lista. Arrivederci") // se il nome non è sulla lista dico di andarsene
 }
