ef2d8e0dee5c13a0f3dad3c01be93a8feea5ee09
gabrisca <gabrielescarparo1985@gmail.com>
2021-04-09 13:00:44 +0200

    prettier

--- Jsnack5/script.js
+++ Jsnack5/script.js
@@ -1,13 +1,15 @@
 // CREARE UN ARRAY VUOTO. CHIEDERE PER 6 VOLTE ALL'UTENTE DI INSERIRE UN NUMERO. SE è DISPARI INSERICILO NELL'ARRAY
 
-var listaNumeri = [ ]
+var listaNumeri = []; // array vuoto
 
-for(var i = 0; i < 6; i++){
+for (var i = 0; i < 6; i++) {
+  // ciclo for di 6
   var nuovoNumero = parseInt(prompt("inserisci un numero")); // variabile nuovo numero
   // console.log(nuovoNumero)
-  if (nuovoNumero%2){
-    listaNumeri.push(nuovoNumero)
+  if (nuovoNumero % 2) {
+    // se nuovo numero dispari lo pusho nell'array
+    listaNumeri.push(nuovoNumero);
   }
 }
 
-console.log(listaNumeri)
+console.log(listaNumeri); // output
