79a7db463433cfd7d97a65deddb13423e8d8c08e
gabrisca <gabrielescarparo1985@gmail.com>
2021-04-13 15:55:57 +0200

    snack8

--- Jsnack8/script.js
+++ Jsnack8/script.js
@@ -8,7 +8,7 @@ console.log("-------")
 console.log("Il numero che hai inserito è lungo " + numero.toString().length + " cifre"); // .toString mi restituisce l'output della proprietà lenght applicata ad un numero
 console.log("-------")
 
-if (numero.toString().length < 4) {
+if (numero.toString().length < 4) { // condizione perchè sia valido l'inserimento del numero
   alert("Numero troppo piccolo. Devi inserire un numero di 4 cifre");
 } else if (numero.toString().length > 4) {
   alert("Numero troppo grande. Devi inserire un numero di 4 cifre");
@@ -17,3 +17,14 @@ if (numero.toString().length < 4) {
   console.log("--------")
 }
 
+var cifreNumero = numero.split(''); // .split mi dà il numero suddiviso in cifre
+console.log("il numero è composto dalle cifre " + cifreNumero)
+console.log("--------")
+
+var somma = 0;
+
+for (var i = 0; i < numero.length; i++) { // ciclo for per sommare le cifre
+  somma += parseInt(numero[i]);
+}
+
+console.log("la somma di " + cifreNumero + " è " + somma);
