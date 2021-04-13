3c2cacaad71688c8f8a17abf23d06212c149660b
gabrisca <gabrielescarparo1985@gmail.com>
2021-04-13 12:09:58 +0200

    jsnack6 fixed

--- Jsnack6/script.js
+++ Jsnack6/script.js
@@ -1,10 +1,15 @@
 // STAMPA IL CUBO DEI PRIMI N NUMERI DOVE N è UN NUMERO INDICATO DALL'UTENTE
 
 
-var numeroIserito = prompt("Inserisci un numero");
+var numeroIserito = parseInt(prompt("Inserisci un numero"));
 // var numeroIserito = 5;
 console.log(numeroIserito);
 
+var somma = 0; // flag
+
 for (var i = 1; i <= numeroIserito; i++){
-  console.log(Math.pow(i,3));
+  var numeriCubici = console.log(Math.pow(i,3));
+  somma += Math.pow(i,3); // += necessario per commare tutti i cubi
 }
+
+console.log("la somma dei cubi è " + somma)
